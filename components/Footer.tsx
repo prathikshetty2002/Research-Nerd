8668272f344141614ac421836967c9c4cfe5f3f7
Prathik Shetty <prathikkshetty15@gmail.com>
2022-03-19 02:55:52 +0530

    components folder appended

---
+++ components/Footer.tsx
@@ -0,0 +1,14 @@
+
+const Footer: React.FC = () => {
+    return <footer className="flex">
+    {/* <a
+      href="https://github.com/Mr-Jayesh/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+        Jayesh Potlabattini
+    </a> */}
+  </footer>
+}
+
+export default Footer
